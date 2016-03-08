a5e41895494556ccddd8c94bb255394efa4388ff
tianyouw <tian.you.w@outlook.com>
2016-03-07 19:48:27 -0500

    Mapview is implemented properly. Also added a button to change between mapview and the smoothed Z acceleration graph.

--- app/src/main/java/ca/uwaterloo/lab3_201_04/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/lab3_201_04/MainActivity.java
@@ -1,9 +1,13 @@
 package ca.uwaterloo.lab3_201_04;
 
+import android.os.Environment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -13,12 +17,17 @@ import android.widget.TextView;
 import android.view.View;
 
 import ca.uwaterloo.sensortoy.LineGraphView;
+import ca.uwaterloo.mapper.*;
 
 import java.util.Arrays;
 import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
     LineGraphView graph;
+    Mapper mv;
+    PedometerMap map;
+    MapLoader ml = new MapLoader();
+
     double bearingRadian = 0;
 
     @Override
@@ -27,11 +36,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         graph = new LineGraphView(getApplicationContext(), 1000, Arrays.asList("X", "Y", "Z"));
+        mv = new Mapper(getApplicationContext(), 1440, 1000, 45, 45);
+        registerForContextMenu(mv);
+        map = ml.loadMap(getExternalFilesDir(null), "E2-3344.svg");
+        mv.setMap(map);
+        registerForContextMenu(mv);
 
         LinearLayout linLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.FrameLayout);
 
-        linLayout.addView(graph);
-        graph.setVisibility(View.VISIBLE);
+        frameLayout.addView(graph);
+        graph.setVisibility(View.INVISIBLE);
+        frameLayout.addView(mv);
+        mv.setVisibility(View.VISIBLE);
 
         TextView stepText = new TextView(this);
         linLayout.addView(stepText);
@@ -55,13 +72,18 @@ public class MainActivity extends AppCompatActivity {
         sensorManager.registerListener(orientationListener, magSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         // Setting up the reset button for the graph.
-        Button btnReset = new Button(this);
+        final Button btnReset = new Button(this);
         btnReset.setText("Clear Displacement");
         linLayout.addView(btnReset);
 
-        Button btnCalib = new Button(this);
+        final Button btnCalib = new Button(this);
         btnCalib.setText("Calibration");
         linLayout.addView(btnCalib);
+
+        final Button btnSwitch = new Button(this);
+        btnSwitch.setText("Switch to Graph");
+        linLayout.addView(btnSwitch);
+
 
         btnCalib.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -79,6 +101,31 @@ public class MainActivity extends AppCompatActivity {
                 accValues.stepCountEast = 0;
             }
         });
+
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(graph.getVisibility() == View.VISIBLE) {
+                    graph.setVisibility(View.INVISIBLE);
+                    mv.setVisibility(View.VISIBLE);
+                    btnSwitch.setText("Switch to graph");
+                } else {
+                    graph.setVisibility(View.VISIBLE);
+                    mv.setVisibility(View.INVISIBLE);
+                    btnSwitch.setText("Switch to Map");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        mv.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        return super.onContextItemSelected(item) || mv.onContextItemSelected(item);
     }
 
     class StepSensorEventListener implements SensorEventListener{
@@ -124,12 +171,12 @@ public class MainActivity extends AppCompatActivity {
 
         public void onSensorChanged(SensorEvent se){
             if(se.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-                // TODO: Add grav/acc smoothing here if not smoothing orientation afterwards.
+                // TODO: Add grav/acc smoothing.
                 gravity[0] = se.values[0];
                 gravity[1] = se.values[1];
                 gravity[2] = se.values[2];
             } else if(se.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-                // TODO: Add mag smoothing here if not smoothing orientation afterwards.
+                // TODO: Add mag smoothing.
                 magnetic[0] = se.values[0];
                 magnetic[1] = se.values[1];
                 magnetic[2] = se.values[2];
